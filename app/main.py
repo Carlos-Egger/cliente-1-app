9d138d373d24b5f549eeb32d9ae2daee68a36b7a
Carlos Egger <carlos.eggern@gmail.com>
2025-07-12 06:50:47 -0300

    change book name

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def get_livros():
     conn = sqlite3.connect('livros.db')
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS livros (id INTEGER PRIMARY KEY, titulo TEXT)')
-    cursor.execute('INSERT OR IGNORE INTO livros (id, titulo) VALUES (1, "1984"), (2, "Dom Casmurro")')
+    cursor.execute('INSERT OR IGNORE INTO livros (id, titulo) VALUES (1, "Noites Brancas"), (2, "Dom Casmurro")')
     conn.commit()
     livros = cursor.execute('SELECT * FROM livros').fetchall()
     conn.close()
